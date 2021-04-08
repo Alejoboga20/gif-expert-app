22c0b17d2b340efacdff9dcaa52c64afdda2c783
alejoboga <alejoboga20@gmail.com>
2021-04-08 15:55:20 -0300

    Setting Up Tests

---
+++ src/tests/GifExpertApp.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+import GifExpertApp from '../GifExpertApp';
+
+describe('GifExpertApp Tests', () => {
+  let wrappper = shallow(<GifExpertApp />);
+
+  test('should Display GifExpertApp properly', () => {
+    expect(wrappper).toMatchSnapshot();
+  });
+});
