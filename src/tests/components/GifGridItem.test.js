f3920879e451d769238b338ace565012b3bf0b46
alejoboga <alejoboga20@gmail.com>
2021-04-08 16:09:34 -0300

    Adding propTypes and update snapshots

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -6,6 +6,13 @@ import GifGridItem from '../../components/GifGridItem';
 describe('GifGridItem Tests', () => {
   let wrappper = shallow(<GifGridItem />);
 
+  const title = 'testing';
+  const url = 'testing';
+
+  beforeEach(() => {
+    wrappper = shallow(<GifGridItem title={title} url={url} />);
+  });
+
   test('should Display GifGridItem properly', () => {
     expect(wrappper).toMatchSnapshot();
   });
