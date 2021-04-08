9219ec56daf34cf416aeb83bcb92162cf1fb0a58
alejoboga <alejoboga20@gmail.com>
2021-04-08 10:34:41 -0300

    Adding GifGrid Item

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 - Use of [GIPHY-API](https://developers.giphy.com/dashboard/)
 - Using props to communicate between components. Adding validations with propTypes.
+- useEffect hook to make an http request.
+- Destructuring Props.
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import GifGridItem from './GifGridItem';
 
 const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
   useEffect(() => {
     getGifs();
-  }, []);
+  }, []); /* Dejar la lista de dependencias vacia hace que el useEffect se ejecute solo la primera vez que reenderizo el componente */
 
   const getGifs = async () => {
     const url =
@@ -19,11 +21,17 @@ const GifGrid = ({ category }) => {
       };
     });
     console.log(gifs);
+    setImages(gifs);
   };
 
   return (
     <div>
       <h3>{category}</h3>
+      <ol>
+        {images.map((image) => (
+          <GifGridItem key={image.id} {...image} />
+        ))}
+      </ol>
     </div>
   );
 };
