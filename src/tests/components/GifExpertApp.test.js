1d1372b2b93a9d41d93e670518f7333272e56ebd
alejoboga <alejoboga20@gmail.com>
2021-04-09 14:28:31 -0300

    Adding tests for GifExpertApp

--- src/tests/components/GifExpertApp.test.js
+++ src/tests/components/GifExpertApp.test.js
@@ -4,9 +4,16 @@ import { shallow } from 'enzyme';
 import GifExpertApp from '../../GifExpertApp';
 
 describe('GifExpertApp Tests', () => {
-  const wrappper = shallow(<GifExpertApp />);
+  let wrappper = shallow(<GifExpertApp />);
 
   test('should Display GifExpertApp properly', () => {
     expect(wrappper).toMatchSnapshot();
+  });
+
+  test('should show a list of categories', () => {
+    const categories = ['Avatar', 'Lord Of Rings'];
+    wrappper = shallow(<GifExpertApp defaultCategories={categories} />);
+    expect(wrappper).toMatchSnapshot();
+    expect(wrappper.find('GifGrid').length).toBe(categories.length);
   });
 });
