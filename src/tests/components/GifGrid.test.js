b04b0d32ee657e22923634631f386516b2163d27
alejoboga <alejoboga20@gmail.com>
2021-04-09 14:22:13 -0300

    Adding tests for checking components existence

--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -23,8 +23,10 @@ describe('GifGrid Tests', () => {
       }
     ];
 
-    useFetchGifs.mockReturnValue({ data: [gifs], loading: false });
+    useFetchGifs.mockReturnValue({ data: gifs, loading: false });
     const wrapper = shallow(<GifGrid category={category} />);
     expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('p').exists()).toBe(false);
+    expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
   });
 });
