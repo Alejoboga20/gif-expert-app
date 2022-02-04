dec46ebc767512b1a89717173f359c15c17ce4c5
Alejo Boga <alejoboga20@gmail.com>
2022-02-04 16:25:47 -0300

    Adding next ui components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom';
 import GitExpertApp from './GifExpertApp';
+import { NextUIProvider } from '@nextui-org/react';
 import './index.css';
 
-ReactDOM.render(<GitExpertApp />, document.getElementById('root'));
+ReactDOM.render(
+	<NextUIProvider>
+		<GitExpertApp />
+	</NextUIProvider>,
+	document.getElementById('root')
+);
