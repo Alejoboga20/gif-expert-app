1b6662c631919daab87a711ff7428ad779bca3df
Alejo Boga <alejoboga20@gmail.com>
2022-02-14 10:40:20 -0300

    Updating test

--- src/tests/components/GifGrid.test.tsx
+++ src/tests/components/GifGrid.test.tsx
@@ -6,28 +6,27 @@ import GifGrid from '../../components/GifGrid/GifGrid';
 jest.mock('../../hooks/useFetchGifs');
 const mockedUseFetchGifs = useFetchGifs as jest.MockedFunction<typeof useFetchGifs>;
 
+const gifs = [
+	{
+		id: 'ABC',
+		url: 'https://localhost.com',
+		title: 'Testing',
+	},
+];
+
 describe('GifGrid Tests', () => {
 	const category = 'Avatar';
 
-	test('should be render properly', () => {
+	test('should be render properly when is loading', () => {
 		mockedUseFetchGifs.mockReturnValue({ data: [], loading: true });
 		const wrapper = shallow(<GifGrid category={category} />);
 		expect(wrapper).toMatchSnapshot();
 	});
 
 	test('should show items when images are loaded with useFetchGifs', () => {
-		const gifs = [
-			{
-				id: 'ABC',
-				url: 'https://localhost.com',
-				title: 'Testing',
-			},
-		];
-
 		mockedUseFetchGifs.mockReturnValue({ data: gifs, loading: false });
 		const wrapper = shallow(<GifGrid category={category} />);
 		expect(wrapper).toMatchSnapshot();
-		expect(wrapper.find('p').exists()).toBe(false);
 		expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
 	});
 });
